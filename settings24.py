d8b132924bbf35b56350923e729eb10bf800f491
Vincenzo-Vengeance <134833962+Vincenzo-Vengeance@users.noreply.github.com>
2025-01-07 14:04:53 +0300

    Add files via upload

---
+++ settings24.py
@@ -0,0 +1,13 @@
+import os
+
+valid_email = 'bochkova03@gmail.com'
+valid_password = 'Moment2020'
+
+not_valid_email = 'bochkova3@mail.com'
+not_valid_password = 'ERFi'
+
+vacant_email = 'lucky@mail.com'
+vacant_password = 'duewp234'
+
+invalid_auth_key = {'key': '6789a2c8124kf41p9a25k2634190c1f1000p9f41be82d98c85ac13682'}
+
